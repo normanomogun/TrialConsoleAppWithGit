af6e96026896fb5b09c7e5e58cdbc154ead8305a
norge <norgee231@yahoo.com>
2019-08-18 08:37:47 +0100

    Add project files.

---
+++ TrialConsoleAppWithGit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TrialConsoleAppWithGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi My Name is Shanice");
+            Console.WriteLine("I am here to help");
+            Console.WriteLine("What is your name?");
+            var usersName = Console.ReadLine();
+            
+            Console.WriteLine($"Hi {usersName}, how may i help today.");
+            Console.WriteLine($"Today is a sunny day and todays date is {DateTime.Now}");
+        }
+    }
+}
