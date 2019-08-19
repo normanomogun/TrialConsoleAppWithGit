28e4f42d2c4f05e266390033b7b565d03b5e1045
norge <norgee231@yahoo.com>
2019-08-19 06:18:40 +0100

    added swimming

--- TrialConsoleAppWithGit/Program.cs
+++ TrialConsoleAppWithGit/Program.cs
@@ -14,6 +14,7 @@ namespace TrialConsoleAppWithGit
             Console.WriteLine($"Hi {usersName}, how may i help today.");
             Console.WriteLine($"Today is a sunny day and todays date is {DateTime.Now}");
             Console.WriteLine("Do you like ice cream, i can search for where to get the best ice cream around you");
+			Console.WriteLine("Do you like swimming, i can search for places around you");
         }
     }
 }
